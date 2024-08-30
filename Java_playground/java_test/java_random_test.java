70caa037713e986eb88bddd6fa9e92101697dce2
HebePython <henrik.bergman.edu@gmail.com>
2024-08-30 14:41:52 +0200

    added player names variables.

--- Java_playground/java_test/java_random_test.java
+++ Java_playground/java_test/java_random_test.java
@@ -25,31 +25,33 @@ public class java_random_test {
             
             int player1Dice = randomMethod();
             int player2Dice = randomMethod();
+            String player1Name = playerNames();
+            String player2Name = playerNames();
 
-            System.out.println("The current score is\n Player 1: " + player1Points + "\n Player 2: " + Player2Points);
+            System.out.println("The current score is\n" + player1Name + ": " + player1Points + "\n" + player2Name + ": " + Player2Points);
             Scanner usrInput = new Scanner(System.in);
             System.out.println("Would you like to roll the dice? y or n: ");
             String yesOrNo = usrInput.nextLine();
             if (yesOrNo.equals("y")) {
-                System.out.println("Player 1 rolls the dice and gets a " + player1Dice + "\nPlayer 2 rolls the dice and gets a  " + player2Dice);
+                System.out.println(player1Name + "rolls the dice and gets a " + player1Dice + "\n" + player2Name + "rolls the dice and gets a  " + player2Dice);
             } else {
                 System.out.println("Thank you for playing, good bye..");
                 System.exit(0);
             }
             if (player1Dice > player2Dice) {
                 player1Points += 1;
-                System.out.println("Player 1 wins and gets 1 point!");
+                System.out.println(player1Name + " wins and gets 1 point!");
             } else if (player1Dice < player2Dice) {
                 Player2Points += 1;
-                System.out.println("Player 2 wins and get 1 point!");
+                System.out.println(player2Name + " wins and get 1 point!");
             } else {
                 System.out.println("It's a tie! No points are awarded");
             }
             if (player1Points == 5) {
-                System.out.println("Player 1 wins! Good game!");
+                System.out.println(player1Name + " wins it all! Good game!");
                 break;
             } else if (Player2Points == 5) {
-                System.out.println("Player 2 wins! Good game!");
+                System.out.println(player2Name + "wins it all! Good game!");
                 break;
             } else; {
                 continue;
