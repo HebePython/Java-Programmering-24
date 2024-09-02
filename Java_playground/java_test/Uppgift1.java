e11d367bc4167d6b1159f62bddc681825b702e4d
HebePython <50890910+HebePython@users.noreply.github.com>
2024-09-02 13:27:08 +0200

    added new file. Uppgift1 klar!

---
+++ Java_playground/java_test/Uppgift1.java
@@ -0,0 +1,23 @@
+package Java_playground.java_test; //Skriv ett program som tar emot ett nummer från user och skriver ut i terminalen om det är positivt, negativt eller noll.
+import java.util.Scanner;
+
+public class Uppgift1 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a number: ");
+        int usrNum = sc.nextInt();
+        sc.close();
+
+        if (usrNum >= 1) {
+            System.out.println("The number is positive.");   
+        } else if (usrNum <= -1) {
+            System.out.println("The number is negative.");   
+        } else {
+            System.out.println("The number is 0.");   
+        }
+
+    }
+
+    
+}
