90d6641da291cef67d3b200ecdf58e6c6d20400e
HebePython <henrik.bergman.edu@gmail.com>
2024-09-16 09:36:18 +0200

    print 2nd game

--- Java_playground/java_test/learningConstructors/main.java
+++ Java_playground/java_test/learningConstructors/main.java
@@ -5,5 +5,7 @@ public class main {
     public static void main(String[] args) {
         videoGameContainer games = new videoGameContainer();
         System.out.println(String.format("Game title: %s\nRelease year: %d\nGame score: %2.1f", games.game1.title, games.game1.releaseYear, games.game1.score));
+        System.out.println(String.format("Game title: %s\nRelease year: %d\nGame score: %2.1f", games.game2.title, games.game2.releaseYear, games.game2.score));
+
     }
 }
