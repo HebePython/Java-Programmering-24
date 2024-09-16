f1f72e06bbe6dab6c6b2e7b71bb0abab55231b6c
HebePython <henrik.bergman.edu@gmail.com>
2024-09-16 09:25:06 +0200

    changed msg.format to string.format

--- Java_playground/java_test/learningConstructors/main.java
+++ Java_playground/java_test/learningConstructors/main.java
@@ -4,6 +4,6 @@ public class main {
     
     public static void main(String[] args) {
         videoGameContainer games = new videoGameContainer();
-        System.out.println(games.game1.title);
+        System.out.println(String.format("Game title: %s\nRelease year: %d\nGame score: %2.1f", games.game1.title, games.game1.releaseYear, games.game1.score));
     }
 }
