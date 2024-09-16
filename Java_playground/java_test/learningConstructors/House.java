00252311ae9cc16bb3792807929f6b7a886c8456
HebePython <50890910+HebePython@users.noreply.github.com>
2024-09-16 20:04:22 +0200

    more updates

--- Java_playground/java_test/learningConstructors/House.java
+++ Java_playground/java_test/learningConstructors/House.java
@@ -19,10 +19,52 @@ public class House {
         
     }
 
-    public void printMyHouseDetails() {
+    House(String homeAdress){
+        this.adress = homeAdress;
+    }
+
+    public void printMyDetails() {
         System.out.println(String.format("The house has %d square meters\nIt has %d floors\nIt's worth $%d\nIt was built in %d\nAdress is %s", this.squareMeters, this.floors, this.worth, this.year, this.adress));
     }
     
+}
+
+class MyCar {
+    String carModel = "Ford";
+    int carWorth = 10000;
+    double carKM = 122.6; //miles
+
+    myCar (String carModel, int carWorth, double carKM) {
+        this.carModel = carModel;
+        this.carWorth = carWorth;
+        this.carKM = carKM;
+    }
+}
+
+class Person {
+    String name = "Johanna Johansson";
+    String occupation = "Student";
+    int age = 20;
+    char sex = 'F';
+    double length = 1.80;
+    String homeAdress = "";
+
+    Person (String name, String occupation, int age, char sex, double length, String homeAdress) {
+        this.name = name;
+        this.occupation = occupation;
+        this.age = age;
+        this.sex = sex;
+        this.length = length;
+        this.homeAdress = homeAdress;
+    }
+
+    Person () {
+
+    }
+
+    public void printMyDetails() {
+        System.out.println(String.format("Person is called %s\nPerson's occupation is %s\nPerson is %d years old\nPerson's sex is %c\nPerson is %1.2fcm long\nPerson lives on %s", this.name, this.occupation, this.age, this.sex, this.length, this.homeAdress));
+    }
 }
 
 
@@ -30,12 +72,20 @@ public class House {
 class main{
 
     public static void main(String[] args) {
-        House myHouse1 = new House(300, 3, 150000, 1991, "22 Leighton str.");
-        myHouse1.printMyHouseDetails();
-        House myHouse2 = new House();
-        myHouse2.printMyHouseDetails();
-        House myHouse3 = myHouse1;
-        myHouse3.printMyHouseDetails();
+
+        House myHouse1 = new House(300, 3, 250000, 1991, "22 Leighton str.");
+        House myHouse2 = new House(250, 2, 175000, 2011, "55 Straton road");
+
+        Person myPerson1 = new Person("David Davidsson", "Gardener", 33, 'M', 1.92, myHouse2.adress);
+        Person myPerson2 = new Person("Harry Harrysson", "Software tester", 39, 'M', 1.74, myHouse1.adress);
+        Person myPerson3 = new Person("Sally Sallydottir", "Singer", 22, 'F', 1.68, myHouse2.adress);
+
+
+        myPerson1.printMyDetails();
+        myPerson2.printMyDetails();
+        myPerson3.printMyDetails();
+  
+  
 
     }
 }
