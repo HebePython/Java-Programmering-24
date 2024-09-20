f4c94d960bb3c52197988311f19ad28623d60ff1
HebePython <henrik.bergman.edu@gmail.com>
2024-09-20 17:25:37 +0200

    added file for E4 exercise

---
+++ Java_playground/java_test/Exercises_Challenges/E4Methods/MainMethodKeywords.java
@@ -0,0 +1,18 @@
+package Java_playground.java_test.Exercises_Challenges.E4Methods;
+
+public class MainMethodKeywords {
+    public static void main(String arguments[]) {
+        System.out.println("main is working fine!");
+        
+    }
+    
+}
+
+//a - it works
+//b private, makes it so jvm can't access the main method?
+//c it works fine without static
+//d eftersom metoden inte returnerar någon int. bara en print så behöver void.
+//e Yes
+//f en string kan fungera som en sorts array. men inte int t.ex. int är en primitiv type?
+//g, YEP
+
