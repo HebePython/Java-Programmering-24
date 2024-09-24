a94bd5ed07c11ff5c297083288f7f7a0516126c3
HebePython <henrik.bergman.edu@gmail.com>
2024-09-24 10:56:01 +0200

    added, finished isValid 4.4c

--- Java_playground/java_test/Exercises_Challenges/E4Methods/StringManipulation/myStringMethods.java
+++ Java_playground/java_test/Exercises_Challenges/E4Methods/StringManipulation/myStringMethods.java
@@ -9,7 +9,33 @@ public class myStringMethods {
         }
         return revString;
     }
+
+    public int countCharacters(String s, char c) {
+        int charCount = 0;
+        for (int i = 1; i <= s.length(); i++) {
+            if (s.charAt(i - 1) == c) {
+                charCount++;
+            }
+        }
+        return charCount;
+    }
     
+    public boolean isValidEmail(String s) {
+
+        String[] domains = {".com", ".edu", ".org"};
+        for (int i = 1; i <= s.length(); i++) {
+            if (s.charAt(i - 1) == '@') {
+                for (int j = 1; j <= domains.length; j++) {
+                    if (s.endsWith(domains[j - 1])) {
+                        return true;
+                    }
+                }
+
+            }
+        }
+
+        return false;
+    }
 }
 
 class Main {
@@ -17,8 +43,11 @@ class Main {
     public static void main(String[] args) {
         
         myStringMethods temp = new myStringMethods();
+        String s = "Hello@gmail.edu";
+        System.out.println(temp.reverseString(s));
+        System.out.println(temp.countCharacters(s, 'l'));
 
-        System.out.println(temp.reverseString("Hello"));
+        System.out.println(temp.isValidEmail(s));
 
     }
 }
